629ec6608130ba10080318f9a4ee1c37e53207bb
Nick <nt1124@my.bristol.ac.uk>
2014-10-19 18:33:23 +0100

    Progress on RSA OT

--- crypto/OT/otSemiHonestRSA.c
+++ crypto/OT/otSemiHonestRSA.c
@@ -5,30 +5,37 @@ virtual int 	listen(unsigned char *buffer, int bufferSize) = 0;
 virtual int 	transfer() = 0;
 */
 
-#include <openssl/rsa.h>
+// #include <openssl/rsa.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../rsa.c"
 
 
-const int padding = RSA_PKCS1_PADDING;
+// const int padding = RSA_PKCS1_PADDING;
 
 
-void send()
+
+
+void send(unsigned char *input0Bytes, unsigned char *input1Bytes, int inputLengths)
 {
+	struct rsaPubKey *PK0, *PK1;
+	mpz_t *input0, *input1;
+	mpz_t *enc0Num, *enc1Num;
+
+
+	convertBytesToMPZ(input0, input0Bytes, inputLengths);
+	convertBytesToMPZ(input1, input1Bytes, inputLengths);
+
+	enc0Num = encRSA(*input0, PK0);
+	enc1Num = encRSA(*input1, PK1);
+
+	// We get PK0, PK1 from receiver here.
 
 }
 
 
 int main()
 {
-	RSA *r;
-	unsigned char message[2048/8] = "123456789012345678901234567890\0";
-	unsigned char *encrypted = (unsigned char*) calloc(2048/8, sizeof(unsigned char));
 
-	generate_key(r);
-	// public_encrypt(message, strlen(message), r, encrypted);
-	private_decrypt(message, strlen(message), r, encrypted);
-
-	return 0;
 }
